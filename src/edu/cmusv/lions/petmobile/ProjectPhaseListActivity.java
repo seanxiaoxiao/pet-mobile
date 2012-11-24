e22c2513abfadc272640223efaea8aa5130e7b5e
Sumeet Kumar <kr.sumeet@gmail.com>
2012-11-23 22:26:43 -0800

    added logic to show project and projec phases

--- src/edu/cmusv/lions/petmobile/ProjectPhaseListActivity.java
+++ src/edu/cmusv/lions/petmobile/ProjectPhaseListActivity.java
@@ -2,32 +2,44 @@ package edu.cmusv.lions.petmobile;
 
 import java.util.Map;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+import edu.cmusv.lions.petmobile.domain.Project;
+import edu.cmusv.lions.petmobile.domain.ProjectPhase;
 
 public class ProjectPhaseListActivity extends PetListActivity {
-
+	
+	private String mProjectId;
+	
 	@Override
-	protected Class<?> getConstantsClass() {
-		// TODO Auto-generated method stub
-		return null;
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setTitle("Project Phases");
 	}
-
+	
+	protected Class<?> getConstantsClass() {
+		return ProjectPhase.class;
+	}
+	
 	@Override
 	protected void requestJsonData() {
-		// TODO Auto-generated method stub
-
+		Intent intent = getIntent();
+		mProjectId = intent.getStringExtra(Project.ID);
+		mDataSource.getProjectPhaseListAsync(mProjectId);
 	}
-
-	@Override
+	
 	protected ListAdapter getListAdapter() {
-		// TODO Auto-generated method stub
-		return null;
+		return new SimpleAdapter(this, mList, R.layout.list_item, new String[] { ProjectPhase.ID },
+				new int[] { R.id.list_item_name });
 	}
-
-	@Override
+	
 	protected void onItemSelected(Map<String, String> selectedItemAttributes) {
-		// TODO Auto-generated method stub
-
+		Intent intent = new Intent(getApplicationContext(), ProjectPhaseDetailsActivity.class);
+		intent.putExtra(ProjectPhase.ID, selectedItemAttributes.get(ProjectPhase.ID));
+		intent.putExtra(Project.ID, mProjectId);
+		startActivity(intent);
 	}
 
 }
